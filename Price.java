e9908e5920357a8c930ce55f8a7e6b1c4c3ca655
Felipe Cadar Chamone <cadar@dcc.ufmg.br>
2020-10-20 21:32:47 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -2,14 +2,9 @@ public abstract class Price {
     public abstract int getPriceCode();
 
     public abstract double getCharge(int daysRented);
-    
+
     public int getFrequentRenterPoints(int daysRented) {
-        // Adicionar o trecho de código extraído.
-        // add frequent renter points
-        int frequentRenterPoints = 1;
-        // add bonus for a two day new release rental
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-            frequentRenterPoints++;
-        return frequentRenterPoints;
+        return 1;
     }
+
 }
