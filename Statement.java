6670932cf3fada1087069be2e87002df0844d148
Felipe Cadar Chamone <cadar@dcc.ufmg.br>
2020-10-20 21:52:39 -0300

    Commit 18

--- Statement.java
+++ Statement.java
@@ -1,3 +1,24 @@
-public class Statement {
-    
+import java.util.Enumeration;
+
+public abstract class Statement {
+
+    abstract String header(Customer aCustomer);
+
+    abstract String figure(Rental each);
+
+    abstract String footer(Customer aCustomer);
+
+
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = header(aCustomer);
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            // show figures for each rental
+            result += figure(each);
+        }
+        // add footer lines
+        result += footer(aCustomer);
+        return result;
+    }
 }
