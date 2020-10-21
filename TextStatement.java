a565e44790a48eb6368354d4affcea6e5f894018
Felipe Cadar Chamone <cadar@dcc.ufmg.br>
2020-10-20 21:50:44 -0300

    Commit 17

--- TextStatement.java
+++ TextStatement.java
@@ -1,17 +1,33 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
+
     public String value(Customer aCustomer) {
         Enumeration rentals = aCustomer.getRentals();
-        String result = "Rental Record for " + aCustomer.getName() + "\n";
+        String result = header(aCustomer);
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            // show figures for each rental
+            result += figure(each);
         }
         // add footer lines
-        result += "Amount owed is " + String.valueOf(aCustomer.getTotalCharge()) + "\n";
+        result += footer(aCustomer);
+        return result;
+    }
+
+    public String header(Customer aCustomer){
+        return "Rental Record for " + aCustomer.getName() + "\n"; 
+    }
+
+    public String figure(Rental each){
+        return "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+    }
+
+    public String footer(Customer aCustomer){
+        String result = "Amount owed is " + String.valueOf(aCustomer.getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + " frequent renter points";
         return result;
     }
+
+
 }
